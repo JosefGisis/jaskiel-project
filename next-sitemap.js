30f4c5144806c2a3601769d879330afb69dedd1a
josef gisis <jsquared.gisis@gmail.com>
2024-06-11 15:20:08 -0400

    added sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+module.exports = {
+	siteUrl: "https://thejaskielteam.com",
+	generateRobotsTxt: true,
+	// Add paths to pages that should be included in the sitemap
+	paths: [
+		"/",
+		// Add more paths as needed
+	],
+}
