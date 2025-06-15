aa13b82549cf46248900a4ee94923ab28d0527d4
Josef Gisis <josefgisis@gmail.com>
2025-06-15 11:51:10 -0400

    updated form

--- app/SurveyForm/CompletedMessage.tsx
+++ app/SurveyForm/CompletedMessage.tsx
@@ -1,5 +1,3 @@
-import { FiInbox } from "react-icons/fi"
-import { FiCheckCircle } from "react-icons/fi"
 import { BsSendCheck } from "react-icons/bs"
 
 export default function ({
@@ -8,15 +6,15 @@ export default function ({
 	setCompleted: (completed: boolean) => void
 }) {
 	return (
-		<div className="xl-container flex flex-col items-center gap-8">
+		<div className="xl-container flex flex-col items-center gap-8 !text-black">
 			<BsSendCheck className="text-6xl text-primary" />
 
 			<div className="p-2">
-				<h4 className="section-title">
+				<h4 className="section-title !text-black">
 					Thank you for reaching out to us!
 				</h4>
 			</div>
-			<p className="section-subtitle">
+			<p className="section-subtitle !text-black">
 				We have received your message and will get back to you as soon
 				as possible.
 			</p>
