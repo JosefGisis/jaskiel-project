8eac7d291498b8121f4f33357fe4c95a2f3e693a
Josef Gisis <josefgisis@gmail.com>
2025-08-22 07:36:08 -0400

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+			</head>
+
 			<body>
 				{/* vercel analytics comp */}
 				<Analytics />
